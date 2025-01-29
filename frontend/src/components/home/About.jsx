8c79dbc12dc252ee1d465eaad8d3d7f922cf238c
Soumya <spu.25.nov@gmail.com>
2025-01-29 11:11:40 +0530

    server deleted frontend updated

--- frontend/src/components/home/About.jsx
+++ frontend/src/components/home/About.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const About = () => {
   return (
     <section className="py-16 bg-white">
-      <div className="container mx-auto text-center">
+      <div className="container mx-auto text-center shadow-md p-6 rounded-lg">
         <h2 className="text-3xl font-bold text-gray-800 mb-6">About Us</h2>
         <p className="text-gray-600">
-          Welcome to ResearchBridge, a platform designed to connect mentors and mentees,
-          fostering collaboration and innovation in research.
+        Welcome to ResearchBridge, a platform designed to connect mentors and mentees, fostering collaboration and innovation in research. We bring together passionate researchers and eager learners, enabling knowledge sharing, project collaboration, and meaningful partnerships.
+        At ResearchBridge, we aim to create a community where ideas thrive, opportunities emerge, and research flourishes. Let's connect, collaborate, and innovate together.
         </p>
       </div>
     </section>
--- frontend/src/components/home/Features.jsx
+++ frontend/src/components/home/Features.jsx
@@ -4,19 +4,19 @@ const Features = () => {
     const features = [
         {
           title: "Explore Page",
-          description: "Discover research papers, resources, and insights.",
+          description: " Discover a curated library of research papers, resources, and insights shared by mentors and mentees to fuel your academic and professional growth.",
         },
         {
           title: "Research Post",
-          description: "Exclusive opportunities from mentors to mentees.",
+          description: "Stay updated with exclusive opportunities where mentors share projects and research collaborations, giving mentees a chance to work directly under their guidance.",
         },
         {
           title: "User Dashboard",
-          description: "Manage connections, research progress, and resources.",
+          description: "Tailored to your needs, the dashboard helps you manage connections, track research progress, access shared resources, and stay organized.",
         },
       ];
   return (
-    <section className="py-16 bg-gray-100">
+    <section className="py-16 bg-white-100">
       <div className="container mx-auto text-center">
         <h2 className="text-3xl font-bold text-gray-800 mb-6">What We Offer</h2>
         <div className="flex justify-center space-x-6">
