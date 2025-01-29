c5c66f44f469497fa463835ad2eee1c5796ea018
Anchal Panwar <anchalpanwar17@gmail.com>
2025-01-29 09:17:38 +0530

    server and db created

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err => console.error("MongoDB Connection Error:", err));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Research Bridge API");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
