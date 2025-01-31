6d6b911c2fbe76ba00902b8583fb62eb3ff77d50
Anchal Panwar <anchalpanwar17@gmail.com>
2025-01-31 08:59:54 +0530

    nodejs backend final

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch(err => console.error("MongoDB Connection Error:", err));
 
